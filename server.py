82e1212046dfb21aa6f5b5479ab9a03d52016f3f
Opeiz <ja.opazoy@gmail.com>
2025-04-03 09:52:21 +0200

    Fix base image

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ def generate_image(name, last_name):
 
     # Calculate text position to center it
     # text_position = ((img_width - text_width) // 2, (img_height - text_height) // 2)
-    text_position = ((img_width - text_width) // 2, 600)
+    text_position = ((img_width - text_width) // 2, 570)
 
     # Define text color (white in this example)
     text_color = (169, 44, 42)
